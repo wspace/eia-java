43ed54ddd5ec7aa2a0536b60504cddc6bae92e00
Zule10 <emestefa.11@gmail.com>
2016-10-27 10:32:52 -0500

    Update interprete.java

--- src/interprete.java
+++ src/interprete.java
@@ -1,92 +1,102 @@
 package whitespace;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Scanner;
 import java.util.Stack;
 
-public class interprete {
-	/*String[] stack = { "ss", "ses", "set", "see" };
-	String[] arit = { "tsss", "tsst", "tsse", "tsts", "tstt" };
-	String[] heap = { "tts", "ttt" };
-	String[] flow = { "ess", "est", "ese", "ets", "ett", "ete", "eee" };
-	String[] io = { "tess", "test", "tets", "tett" };*/
-	static ArrayList<String> linea = new ArrayList<String>();
-	static Stack<String> temp = new Stack<String>();
+public class whitespace {
+
+	static ArrayList<Integer> li = new ArrayList<Integer>();
+	static ArrayList<Integer> temp = new ArrayList<Integer>();
 	static Stack<Character> as = new Stack<Character>();
+	static int l = -1;
 
-	public static void interpretar(String cadena) {
-		linea(cadena);
-	}
-
-	//Para toar cada línea y enviar al comando con el cual comienza
-	private static void linea(String cadena) {
-		String c = null;
-		int l = 0;
-		int n=0;
-		for (int i = 0; i < cadena.length(); i++) {
-			l=0;
-			if (cadena.startsWith("ss")) {
-				while (cadena.charAt(l) != 'e') {
-					l++;
-				}
-				c = cadena.substring(i, (l + 1));
-				linea.add(c);
-				pushNum(cadena);
-				n+=1;
-				cadena=cadena.substring(l+1, cadena.length());
-				i=0;
-			} else if(cadena.startsWith("tess")) {
-				if(n!=0) {
-					System.out.print(as.get(n-1).toString());
-				}
-				cadena=cadena.substring(3,cadena.length());
-				// llamar switch demás comandos
-				i=0;
-			}
-			//i=0;
-			l++;
-			//cadena=cadena.substring(l, cadena.length());
-			
-		}
-	}
-	
-	private static void pushNum(String cadena) {
-		while (!linea.isEmpty()) {
-			String m = linea.get(0).substring(1, linea.get(0).length());
-			as.push(ascii(m));
-			linea.remove(0);
-		}
-	}
-
-	private static Character ascii(String temp) {
+	private static void evaluar() {
 		// TODO Auto-generated method stub
 		int asc = 0;
-		for (int i = 0; i < temp.length(); i++) {
-			if (temp.startsWith("s")) {
-				if ((temp.charAt(i)) == 's') {
+		int i = 0;
+		while (!li.isEmpty()) {
+			int f = li.get(0);
+			if (f == 32) {
+				while (li.get(0) != 10) {
+					temp.add(li.remove(0));
+				}
+				li.remove(0);
+				if (temp.get(1) == 32) {
+					temp.remove(0);
+					temp.remove(1);
+				}
+				as.push(ascii());
+				l++;
+				temp.clear();
+			} else if (f == 9) {
+				int j = 0;
+				while (j <= 4) {
+					temp.add(li.remove(0));
+					j++;
+				}
+				tabulador();
+				temp.clear();
+			}
+			i++;
+		}
+
+	}
+
+	private static void tabulador() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < temp.size() - 1; i++) {
+			if (temp.get(i) == 32 && temp.get(i + 1) == 32) {
+				if (l != -1) {
+					System.out.print(as.get(l).toString());
+				}
+			}
+		}
+	}
+
+	private static Character ascii() {
+		// TODO Auto-generated method stub
+		int asc = 0;
+		for (int i = 0; i < temp.size() - 1; i++) {
+			if (temp.get(0) == 32) {
+				if (temp.get(i) == 32) {
 					asc = asc * 2;
-				} else if ((temp.charAt(i)) == 't') {
+				} else if ((temp.get(i)) == 9) {
 					asc = (asc * 2) + 1;
 				}
-			} else if (i != 0) {
-				if ((temp.charAt(i)) == 's') {
+			} else {
+				if (temp.get(i) == 32) {
 					asc = asc * 2;
-				} else if ((temp.charAt(i)) == 't') {
+				} else if (temp.get(i) == 9) {
 					asc = (asc * (2)) - 1;
 				}
 			}
 		}
-		return (char) asc;
+		if (asc == 1) {
+			return (char) 49;
+		} else {
+			return (char) asc;
+		}
+	}
+
+	public static void leer() {
+		try {
+			FileInputStream ficheroEntrada2 = new FileInputStream(new File(
+					"fichero.bin"));
+			int dato;
+			while ((dato = ficheroEntrada2.read()) != -1) {
+				li.add(dato);
+			}
+			ficheroEntrada2.close();
+		} catch (Exception errorDeFichero) {
+			System.out.println("Ha habido problemas: "
+					+ errorDeFichero.getMessage());
+		}
 	}
 
 	public static void main(String[] args) {
-
-		/*Scanner sc = new Scanner(System.in);
-		String b = sc.nextLine();
-		interpretar(b);*/
-		interpretar("sssttttstsetessssstttststetesssssttsttssetesssssttsststetesssssttstsstetesssssttsttstetesssssttstsstetesssssssstsssssetesssssttsststetessssstttssttetessssstttstssetesssssttsststetesssssttssttsetesssssttssttsetesssssttsssstetesssssttstttsetesssssttstttsetesssssttttsstetessssstsssssetesssssttsstttetesssssttsttttetesssssttstttsetesssssttttstsetesssssttsssstetesssssttsttssetesssssttsststetesssssttttstsetessssstsssssetesssssttsttstetessssstttststetesssssttstttsetesssssttstsstetesssssttsttttetesssssttttstsetess");
-		//Mi nombre : sssttttstsetessssstttststetesssssttsttssetesssssttsststetesssssttstsstetesssssttsttstetesssssttstsstetesssssssstsssssetesssssttsststetessssstttssttetessssstttstssetesssssttsststetesssssttssttsetesssssttssttsetesssssttsssstetesssssttstttsetesssssttstttsetesssssttttsstetessssstsssssetesssssttsstttetesssssttsttttetesssssttstttsetesssssttttstsetesssssttsssstetesssssttsttssetesssssttsststetesssssttttstsetessssstsssssetesssssttsttstetessssstttststetesssssttstttsetesssssttstsstetesssssttsttttetesssssttttstsetess
-
+		leer();
+		evaluar();
 	}
 }
